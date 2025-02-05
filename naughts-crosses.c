8a2f1ad4da63d21ae31a772b253b82cd98edc5ac
Ben Kazumasa <196516609+ben-kazumasa@users.noreply.github.com>
2025-02-05 23:50:14 +1100

    read user input without validation

--- naughts-crosses.c
+++ naughts-crosses.c
@@ -5,20 +5,54 @@ MADE WITHOUT ANY GUIDE
 */
 
 #include <stdio.h>
+#define GRID_SIZE 9
+#define TOP_LEFT 1
+#define TOP_MID 2
+#define TOP_RIGHT 3
+#define MID_LEFT 4
+#define MID_MID 5
+#define MID_RIGHT 6
+#define BOT_LEFT 7
+#define BOT_MID 8
+#define BOT_RIGHT 9
+#define INPUT_HELP 0
+#define INPUT_NOTHING -1
+#define p1_SYMBOL 'X'
+#define p2_SYMBOL 'O'
 
 void print_header();
 void print_grid();
 
 int main(void) 
-{
+{   
+    // Stores the grid elements, starting blank
+    char gameGrid[GRID_SIZE] = {' ', ' ', ' ', ' ', ' ', ' '};
+    
+    // Stores the player's input
+    int input = INPUT_NOTHING;
+    //int tempInput = INPUT_NOTHING;
+    // https://www.reddit.com/r/learnprogramming/comments/1d0w4x/c_scanf_d_but_error_if_user_enters_a_character/
+
+    // Stores the current game turn, starting from 1: player 1 has odd, player 2 has even turns
+    int gameTurn = 1;
+
     print_grid();
+    printf("Turn: %d. Make a move: ", gameTurn);
+    scanf("%d", &input);
+    printf("\nYour move was: %d", input);
+    
 }
 
 void print_header() 
 {
     // Name of the game
     printf("\t NAUGHTS AND CROSSES\n");
-    printf("\t*********************\n\n\n");
+    printf("\t*********************\n");
+    printf("  Type '0' for help. Type q to quit.\n");
+    printf("\n\n\n");
+    //printf("Player 1: \n");
+    //printf("Player 2: \n\n\n");
+    //TODO add ability to choose if player goes first or second.
 }
 
 void print_grid() 
