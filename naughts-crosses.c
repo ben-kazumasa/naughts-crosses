24cc0aa270a223de0ac36de0fb6448913600002c
Ben Kazumasa <196516609+ben-kazumasa@users.noreply.github.com>
2026-01-15 18:51:52 +1100

    Print single move onto playgrid area

--- naughts-crosses.c
+++ naughts-crosses.c
@@ -6,15 +6,15 @@ MADE WITHOUT ANY GUIDE
 
 #include <stdio.h>
 #define GRID_SIZE 9
-#define TOP_LEFT 1
-#define TOP_MID 2
-#define TOP_RIGHT 3
-#define MID_LEFT 4
-#define MID_MID 5
-#define MID_RIGHT 6
-#define BOT_LEFT 7
-#define BOT_MID 8
-#define BOT_RIGHT 9
+#define TOP_LEFT 0
+#define TOP_MID 1
+#define TOP_RIGHT 2
+#define MID_LEFT 3
+#define MID_MID 4
+#define MID_RIGHT 5
+#define BOT_LEFT 6
+#define BOT_MID 7
+#define BOT_RIGHT 8
 #define INPUT_HELP 0
 #define INPUT_NOTHING -1
 #define p1_SYMBOL 'X'
@@ -36,27 +36,18 @@ int main(void)
     //int tempInput = INPUT_NOTHING;
     // https://www.reddit.com/r/learnprogramming/comments/1d0w4x/c_scanf_d_but_error_if_user_enters_a_character/
 
-    // Stores the current game turn, starting from 1: player 1 has even, player 2 has odd turns
+    // Stores the current game turn, starting from 0: player 1 has even, player 2 has odd turns
     int gameTurn = 0;
 
-    print_grid();
+    print_grid(gameGrid);
     printf("Turn: %d. Make a move: ", gameTurn);
     scanf("%d", &input);
     printf("\nYour move was: %d\n", input);
 
 
-   add_move(input, gameTurn, gameGrid);
-    
-    // Testing to see if we can input player moves
-    int i;
-    for (i=0; i < GRID_SIZE; i++) {
-        if (!(i % 3)) 
-        {
-            // Divisible by 3: start a new line
-            printf("\n");
-        }
-        printf("Game square %d: %c\t", i, gameGrid[i]);
-    }
+    add_move(input, gameTurn, gameGrid);
+
+    print_grid(gameGrid);
     
 }
 
@@ -72,24 +63,25 @@ void print_header()
     //TODO add ability to choose if player goes first or second.
 }
 
-void print_grid() 
+void print_grid(char gameGrid[]) 
 {
     
     print_header();
 
     // Prints the basic 3x3 grid for the game
     // Each square is 7 spaces across, and 3 lines tall, excluding boundary lines
+    // Also inserts the current played game data (X and O)
 
     printf("\t     ||       ||     \n");
-    printf("\t     ||       ||     \n");
-    printf("\t     ||       ||     \n");
-    printf("      =========================\n");
-    printf("\t     ||       ||     \n");
-    printf("\t     ||       ||     \n");
+    printf("\t %c   ||   %c   ||   %c \n", gameGrid[TOP_LEFT], gameGrid[TOP_MID], gameGrid[TOP_RIGHT]);
     printf("\t     ||       ||     \n");
     printf("      =========================\n");
     printf("\t     ||       ||     \n");
+    printf("\t %c   ||   %c   ||   %c \n", gameGrid[MID_LEFT], gameGrid[MID_MID], gameGrid[MID_RIGHT]);
     printf("\t     ||       ||     \n");
+    printf("      =========================\n");
+    printf("\t     ||       ||     \n");
+    printf("\t %c   ||   %c   ||   %c \n", gameGrid[BOT_LEFT], gameGrid[BOT_MID], gameGrid[BOT_RIGHT]);
     printf("\t     ||       ||     \n");
     
     printf("\n\n");
